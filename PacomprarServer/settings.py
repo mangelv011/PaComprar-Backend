f838ba217da60a7e2e0eec4451244e999e702e32
mangelv011 <miangelvallejo@outlook.com>
2025-05-15 19:06:29 +0200

    cors local running

--- PacomprarServer/settings.py
+++ PacomprarServer/settings.py
@@ -172,8 +172,33 @@ SPECTACULAR_SETTINGS = {
 }
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS settings for allowing the React frontend to make requests
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Default React port
+    "http://localhost:3001",  # Alternative React port
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
+]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 
 SIMPLE_JWT = {
